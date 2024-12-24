8625ce1d4a893e05047f26e54e2d094a15158b13
nehas28 <neha.srinivasa123@gmail.com>
2024-12-23 21:16:35 -0500

    added MongoDB

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:NqDBrHCTABot2EPMyfYm6xO8@172.21.182.199:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
